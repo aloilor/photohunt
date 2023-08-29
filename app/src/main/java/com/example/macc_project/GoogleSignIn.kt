8b4adf0443ecfaa3117dc3ac3116a631a708b147
ChriT99 <christiantedesco99@gmail.com>
2023-08-29 17:54:41 +0200

    Splash Image and Google SignIn

---
+++ app/src/main/java/com/example/macc_project/GoogleSignIn.kt
@@ -0,0 +1,86 @@
+package com.example.macc_project
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.ImageView
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+class GoogleSignIn : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+    private lateinit var googleSignInClient: GoogleSignInClient
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+        auth = Firebase.auth
+
+        val googleButton = findViewById<ImageView>(R.id.googleButton)
+        googleButton.setOnClickListener {
+            signIn()
+        }
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                val account = task.getResult(ApiException::class.java)!!
+                Log.d(TAG, "firebaseAuthWithGoogle:" + account.id)
+                firebaseAuthWithGoogle(account.idToken!!)
+            } catch (e: ApiException) {
+                // Google Sign In failed, update UI appropriately
+                Log.w(TAG, "Google sign in failed", e)
+            }
+        }
+    }
+
+    private fun firebaseAuthWithGoogle(idToken: String) {
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "signInWithCredential:success")
+                    /*val userFirebase = auth.currentUser
+                    if(userFirebase != null) {
+                        addUserToDB
+                    }*/
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "signInWithCredential:failure", task.exception)
+                }
+            }
+    }
+
+    private fun signIn() {
+        val signInIntent = googleSignInClient.signInIntent
+        startActivityForResult(signInIntent, RC_SIGN_IN)
+    }
+
+
+    companion object {
+        const val TAG = "GoogleActivity"
+        private const val RC_SIGN_IN = 9002
+    }
+}
--- app/src/main/java/com/example/macc_project/MainActivity.kt
+++ app/src/main/java/com/example/macc_project/MainActivity.kt
@@ -3,45 +3,28 @@ package com.example.macc_project
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.content.Intent
-import android.widget.Button
-import android.widget.TextView
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.ktx.Firebase
+import android.os.Looper
+import android.os.Handler
+import android.view.WindowManager
+
 
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
 
-        //authentication
-        auth = Firebase.auth
-        val btnGoTo = findViewById<Button>(R.id.btnGoTo)
-        val tvHomepage = findViewById<TextView>(R.id.tvHomepage)
-
-        if (auth.currentUser != null) {
-            val email = auth.currentUser?.email
-            tvHomepage.text = "$email"
-            btnGoTo.text = "Logout"
-        } else {
-            btnGoTo.text = "Login"
-        }
-
-        btnGoTo.setOnClickListener {
-            if (auth.currentUser != null) {
-                auth.signOut()
-                tvHomepage.text = "Homepage"
-                btnGoTo.text = "Login"
-            } else {
-                Intent(this, Login::class.java).also {
-                    startActivity(it)
-                }
-            }
-        }
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, Login::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
 
 
     }
