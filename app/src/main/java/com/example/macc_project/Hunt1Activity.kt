266e594878d9dbd5630f8dd0e45bd7caaea4504c
aloilor <aloisi.lorenzo99@gmail.com>
2023-08-31 11:44:53 +0200

    photos are taken

--- app/src/main/java/com/example/macc_project/Hunt1Activity.kt
+++ app/src/main/java/com/example/macc_project/Hunt1Activity.kt
@@ -1,35 +1,57 @@
 package com.example.macc_project
 
 import android.Manifest
+import android.app.Activity
+import android.content.ActivityNotFoundException
+import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import android.view.View
 import androidx.activity.result.contract.ActivityResultContracts
+import com.example.macc_project.databinding.ActivityHunt1Binding
 import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.storage.FirebaseStorage
+import java.io.ByteArrayOutputStream
 
 
 class Hunt1Activity : AppCompatActivity() {
     private val CAMERA_PERMISSION_CODE = 101
+    private val REQUEST_IMAGE_CAPTURE = 1
+    private lateinit var storage: FirebaseStorage
+
+    private lateinit var binding: ActivityHunt1Binding
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_hunt1)
+        binding = ActivityHunt1Binding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        storage = FirebaseStorage.getInstance()
+
         getPermissions()
+
+        binding.btnPic.setOnClickListener {
+            openCamera()
+        }
+
     }
 
-    fun getPermissions() {
+    private fun getPermissions() {
         // Check and request camera permission if not granted
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
             != PackageManager.PERMISSION_GRANTED
         ) {
             requestPermissions(arrayOf(Manifest.permission.CAMERA), CAMERA_PERMISSION_CODE)
-        }
-        else {
-            Log.w("camera","permission granted")
+        } else {
+            Log.w("camera", "permission granted")
         }
     }
 
@@ -47,4 +69,26 @@ class Hunt1Activity : AppCompatActivity() {
         }
     }
 
+    private fun openCamera() {
+        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        try {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+        } catch (e: ActivityNotFoundException) {
+            // display error state to the user
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == Activity.RESULT_OK) {
+            val imageBitmap = data?.extras?.get("data") as Bitmap
+            binding.image.setImageBitmap(imageBitmap)
+            val fileName = "image_${System.currentTimeMillis()}.jpg"
+            val storageRef = storage.reference.child("images").child(fileName)
+            // Convert the Bitmap to a byte array
+            val baos = ByteArrayOutputStream()
+            imageBitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos)
+            val data = baos.toByteArray()
+        }
+    }
 }
